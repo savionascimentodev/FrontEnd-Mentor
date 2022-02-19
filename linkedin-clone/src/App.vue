b2ca3e0e48d5c9a8f646c29b6d92f2a531972fe4
Savio Nascimento <savionascimentodev@gmail.com>
2022-02-19 18:49:06 -0400

    🎉 init project

---
+++ linkedin-clone/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HelloWorld />
+    <h4>Linkedin Clone</h4>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld }
+}
+</script>
+<style lang="scss">
+@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css';
+</style>
